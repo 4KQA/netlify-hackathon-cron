dec9aca8d853248468a057c4949aa64acace55f5
Lars Albert (AKQA) <lars.albert@akqa.com>
2022-10-31 13:33:38 +0100

    added heartbeat

---
+++ pages/api/heartbeat.js
@@ -0,0 +1,8 @@
+let counter = 0;
+export default function handler(req, res) {
+  counter = counter + 1;
+  const httpCode = Math.floor(Math.random() * (3 - 1 + 1) + 1) == 3 ? 500 : 200; // 33% chance to get 500 status
+  res
+    .status(httpCode)
+    .json({ counter, serverStatus: httpCode == 500 ? "unhappy" : "happy" });
+}
