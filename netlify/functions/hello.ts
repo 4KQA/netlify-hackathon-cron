d84e1bd6a8c94d7fe43294a012e77542119cad2a
Simon Larsson <simon.larsson@akqa.com>
2022-10-31 11:54:31 +0100

    change hello output.

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,9 +1,10 @@
 import { Handler } from "@netlify/functions";
 
 const handler: Handler = async (event, context) => {
+  const date = new Date();
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello World" }),
+      body: JSON.stringify({ message: date.toDateString() }),
   };
 };
 
