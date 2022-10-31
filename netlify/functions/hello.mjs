e2f869da7dda603abdc07df288e8dfd4892b4023
Lars Albert (AKQA) <lars.albert@akqa.com>
2022-10-31 12:24:11 +0100

    Added scheduler

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -1,15 +1,18 @@
 import fetch from "node-fetch";
+import { schedule } from "@netlify/functions";
 
-const handler = async (event, context) => {
+const apiHandler = async (event, context) => {
   const appRequest = await fetch(
     "https://rad-dodol-525b01.netlify.app/api/hello"
   );
   const date = new Date();
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: date.toDateString(), info: "Made request to endpoint" }),
-
+    body: JSON.stringify({
+      message: date.toDateString(),
+      info: "Made request to endpoint",
+    }),
   };
 };
 
-export { handler };
+export const handler = schedule("*/5 * * * * *", apiHandler); // every 5 seconds
