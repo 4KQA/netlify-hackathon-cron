161ef94e60a369aa7b8e4819ed565c2a42f88ed8
Lars Albert (AKQA) <lars.albert@akqa.com>
2022-10-31 12:05:23 +0100

    Added node-fetch request

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,8 +1,14 @@
+import fetch from "node-fetch";
+
 const handler = async (event, context) => {
+  const appRequest = await fetch(
+    "https://rad-dodol-525b01.netlify.app/api/hello"
+  );
   const date = new Date();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: date.toDateString() }),
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: date.toDateString(), info: "Made request to endpoint" }),
+
   };
 };
 
